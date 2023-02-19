27df383af8e3c06b39a1b3ea28012baa5e6b3a31
EmilianoG <90258522+emilianoguardabassi@users.noreply.github.com>
2023-02-19 12:41:50 -0300

    first commit
    First commit

---
+++ groupstage.py
@@ -0,0 +1,12 @@
+from simulacionmundialfifa import *
+
+resultadopartido(argentina,francia)
+
+grupo_a=[ecuador,paisesbajos,senegal,qatar]
+grupo_b=[estadosunidos,gales,inglaterra,iran]
+grupo_c=[argentina,mejico,polonia,saudi]
+grupo_d=[australia,dinamarca,francia,tunez]
+grupo_e=[alemania,costarica,españa,japon]
+grupo_f=[belgica,canada,croacia,marruecos]
+grupo_g=[brazil,camerun,serbia,suiza]
+grupo_h=[corea,ghana,portugal,uruguay]
