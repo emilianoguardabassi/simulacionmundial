36f6b1bc9302fa9e27911c02fbc1dd5d17e611b6
EmilianoG <90258522+emilianoguardabassi@users.noreply.github.com>
2023-02-20 17:03:30 -0300

    Fase de grupos practicamente funcional
    Fase de grupos ahora se ordena por puntos y diferencia de goles. Falta organizar por goles a favor, goles en contra y algun determinante si todo lo anterior coincide

--- groupstage.py
+++ groupstage.py
@@ -20,8 +20,9 @@ def checkresultado(local,vis,game):
         vis.grouppoints+=3
     return local.grouppoints,local.grouppoints
 
-
-def definirgrupo(grupo):
+#define los puntos del grupo
+#falta resolver los empates en puntos
+def definirpuntosgrupo(grupo):
     jugador1=grupo[0]
     jugador2=grupo[1]
     jugador3=grupo[2]
@@ -39,15 +40,84 @@ def definirgrupo(grupo):
     checkresultado(jugador2,jugador3,j2vsj3)
     checkresultado(jugador2,jugador4,j2vsj4)
     checkresultado(jugador3,jugador4,j3vsj4)
+
+    grupo.sort(key=lambda x: x.grouppoints,reverse=True)
+    primerlugar=grupo[0]
+    segundolugar=grupo[1]
+    tercerlugar=grupo[2]
+    cuartolugar=grupo[3]
+
+    return [primerlugar,segundolugar,tercerlugar,cuartolugar]
+    
+def desempatargrupo(listadeganadores):
+    ganadores=[]
+    if listadeganadores[0].grouppoints==listadeganadores[2].grouppoints:
+        ganadores=listadeganadores.sort(key=lambda x: (x.gf-x.gc),reverse=True)
+    elif listadeganadores[0].grouppoints==listadeganadores[1].grouppoints:
+        if (listadeganadores[0].gf-listadeganadores[0].gc)>(listadeganadores[1].gf-listadeganadores[1].gc):
+            ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
+        elif (listadeganadores[0].gf-listadeganadores[0].gc)<(listadeganadores[1].gf-listadeganadores[1].gc):
+            ganadores=[listadeganadores[1],listadeganadores[0],listadeganadores[2],listadeganadores[3]]
+        else:
+            ganadores=listadeganadores.sort(key=lambda x: (x.gf-x.gc),reverse=True)
+    elif listadeganadores[1].grouppoints==listadeganadores[2].grouppoints:
+        if (listadeganadores[1].gf-listadeganadores[1].gc)>(listadeganadores[2].gf-listadeganadores[2].gc):
+            ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
+        elif (listadeganadores[1].gf-listadeganadores[1].gc)<(listadeganadores[2].gf-listadeganadores[2].gc):
+            ganadores=[listadeganadores[0],listadeganadores[2],listadeganadores[1],listadeganadores[3]]
+        else:
+            ganadores=listadeganadores.sort(key=lambda x: (x.gf-x.gc),reverse=True)
+        
+    else:
+        ganadores=listadeganadores
+    
+
+    if listadeganadores[2].grouppoints==listadeganadores[3].grouppoints:
+        if (listadeganadores[2].gf-listadeganadores[2].gc)>(listadeganadores[3].gf-listadeganadores[3].gc):
+            ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
+        elif (listadeganadores[2].gf-listadeganadores[2].gc)<(listadeganadores[3].gf-listadeganadores[3].gc): 
+            ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[3],listadeganadores[2]]
     
     print()
-    print(f"{grupo[0].name}| puntos: {grupo[0].grouppoints} | GF: {grupo[0].gf} | GC: {grupo[0].gc} | DG: {grupo[0].gf - grupo[0].gc}")
-    print(f"{grupo[1].name}| puntos: {grupo[1].grouppoints} | GF: {grupo[1].gf} | GC: {grupo[1].gc} | DG: {grupo[1].gf - grupo[1].gc}")
-    print(f"{grupo[2].name}| puntos: {grupo[2].grouppoints} | GF: {grupo[2].gf} | GC: {grupo[2].gc} | DG: {grupo[2].gf - grupo[2].gc}")
-    print(f"{grupo[3].name}| puntos: {grupo[3].grouppoints} | GF: {grupo[3].gf} | GC: {grupo[3].gc} | DG: {grupo[3].gf - grupo[3].gc}")
+    print("-"*60)
+    print(f"{ganadores[0].name}| puntos: {ganadores[0].grouppoints} | GF: {ganadores[0].gf} | GC: {ganadores[0].gc} | DG: {ganadores[0].gf - ganadores[0].gc}")
+    print(f"{ganadores[1].name}| puntos: {ganadores[1].grouppoints} | GF: {ganadores[1].gf} | GC: {ganadores[1].gc} | DG: {ganadores[1].gf - ganadores[1].gc}")
+    print(f"{ganadores[2].name}| puntos: {ganadores[2].grouppoints} | GF: {ganadores[2].gf} | GC: {ganadores[2].gc} | DG: {ganadores[2].gf - ganadores[2].gc}")
+    print(f"{ganadores[3].name}| puntos: {ganadores[3].grouppoints} | GF: {ganadores[3].gf} | GC: {ganadores[3].gc} | DG: {ganadores[3].gf - ganadores[3].gc}")
+    print("-"*60)
+
+    return ganadores
+
+
+
+
 
 
 
 
 print()
-definirgrupo(grupo_h)
+grupo_a_partidos=definirpuntosgrupo(grupo_a)
+grupo_a_partidos_ordenado=desempatargrupo(grupo_a_partidos)
+
+grupo_b_partidos=definirpuntosgrupo(grupo_b)
+grupo_b_partidos_ordenado=desempatargrupo(grupo_b_partidos)
+
+grupo_c_partidos=definirpuntosgrupo(grupo_c)
+grupo_c_partidos_ordenado=desempatargrupo(grupo_c_partidos)
+
+grupo_d_partidos=definirpuntosgrupo(grupo_d)
+grupo_d_partidos_ordenado=desempatargrupo(grupo_d_partidos)
+
+grupo_e_partidos=definirpuntosgrupo(grupo_e)
+grupo_e_partidos_ordenado=desempatargrupo(grupo_e_partidos)
+
+grupo_f_partidos=definirpuntosgrupo(grupo_f)
+grupo_f_partidos_ordenado=desempatargrupo(grupo_f_partidos)
+
+grupo_g_partidos=definirpuntosgrupo(grupo_g)
+grupo_g_partidos_ordenado=desempatargrupo(grupo_g_partidos)
+
+grupo_h_partidos=definirpuntosgrupo(grupo_h)
+grupo_h_partidos_ordenado=desempatargrupo(grupo_h_partidos)
+
+
