42e2a1d4eb2b80bd887f139fde12340b0743c8ec
EmilianoG <90258522+emilianoguardabassi@users.noreply.github.com>
2023-02-20 18:57:05 -0300

    usable
    Ya se puede simular un mundial entero. Falta añadir el tercer puesto, mejorar el sistema de penales, mejorar la influencia de los stats de los equipos, buscar posibles bugs, hacerlo más placentero a la vista y posiblemente exportarlo a excel.

--- fasedepartidos.py
+++ fasedepartidos.py
@@ -46,27 +46,45 @@ def definirpuntosgrupo(grupo):
     segundolugar=grupo[1]
     tercerlugar=grupo[2]
     cuartolugar=grupo[3]
-
+    
     return [primerlugar,segundolugar,tercerlugar,cuartolugar]
     
 def desempatargrupo(listadeganadores):
-    ganadores=[]
+    ganadores=listadeganadores
     if listadeganadores[0].grouppoints==listadeganadores[2].grouppoints:
-        ganadores=listadeganadores.sort(key=lambda x: (x.gf-x.gc),reverse=True)
+        if (listadeganadores[0].gf-listadeganadores[0].gc)>(listadeganadores[2].gf-listadeganadores[2].gc):
+            if (listadeganadores[0].gf-listadeganadores[0].gc)>(listadeganadores[1].gf-listadeganadores[1].gc):
+                if (listadeganadores[1].gf-listadeganadores[1].gc)>(listadeganadores[2].gf-listadeganadores[2].gc):
+                    ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
+                elif (listadeganadores[1].gf-listadeganadores[1].gc)<(listadeganadores[2].gf-listadeganadores[2].gc):
+                    ganadores=[listadeganadores[0],listadeganadores[2],listadeganadores[1],listadeganadores[3]]
+                else:
+                    ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
+            elif (listadeganadores[0].gf-listadeganadores[0].gc)<(listadeganadores[1].gf-listadeganadores[1].gc):
+                ganadores=[listadeganadores[1],listadeganadores[0],listadeganadores[2],listadeganadores[3]]
+
+        elif (listadeganadores[0].gf-listadeganadores[0].gc)>(listadeganadores[1].gf-listadeganadores[1].gc):
+                if (listadeganadores[1].gf-listadeganadores[1].gc)>(listadeganadores[2].gf-listadeganadores[2].gc):
+                    ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
+                elif (listadeganadores[1].gf-listadeganadores[1].gc)<(listadeganadores[2].gf-listadeganadores[2].gc):
+                    ganadores=[listadeganadores[0],listadeganadores[2],listadeganadores[1],listadeganadores[3]]
+                else:
+                    ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
+
     elif listadeganadores[0].grouppoints==listadeganadores[1].grouppoints:
         if (listadeganadores[0].gf-listadeganadores[0].gc)>(listadeganadores[1].gf-listadeganadores[1].gc):
             ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
         elif (listadeganadores[0].gf-listadeganadores[0].gc)<(listadeganadores[1].gf-listadeganadores[1].gc):
             ganadores=[listadeganadores[1],listadeganadores[0],listadeganadores[2],listadeganadores[3]]
         else:
-            ganadores=listadeganadores.sort(key=lambda x: (x.gf-x.gc),reverse=True)
+            ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
     elif listadeganadores[1].grouppoints==listadeganadores[2].grouppoints:
         if (listadeganadores[1].gf-listadeganadores[1].gc)>(listadeganadores[2].gf-listadeganadores[2].gc):
             ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
         elif (listadeganadores[1].gf-listadeganadores[1].gc)<(listadeganadores[2].gf-listadeganadores[2].gc):
             ganadores=[listadeganadores[0],listadeganadores[2],listadeganadores[1],listadeganadores[3]]
         else:
-            ganadores=listadeganadores.sort(key=lambda x: (x.gf-x.gc),reverse=True)
+            ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
         
     else:
         ganadores=listadeganadores
@@ -77,6 +95,7 @@ def desempatargrupo(listadeganadores):
             ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[2],listadeganadores[3]]
         elif (listadeganadores[2].gf-listadeganadores[2].gc)<(listadeganadores[3].gf-listadeganadores[3].gc): 
             ganadores=[listadeganadores[0],listadeganadores[1],listadeganadores[3],listadeganadores[2]]
+
     
     print()
     print("-"*60)
@@ -88,36 +107,118 @@ def desempatargrupo(listadeganadores):
 
     return ganadores
 
+def ganadordelpartido(player1,player2):
+    goles=resultadopartido(player1,player2)
+    if goles[0]>goles[1]:
+        return player1
 
-
-
+    elif goles[0]<goles[1]:
+        return player2
+        
+    elif goles[0]==goles[1]:
+        ganador=penales(player1,player2)
+        return ganador
+        
+        
+    
 
 
 
 
 print()
 grupo_a_partidos=definirpuntosgrupo(grupo_a)
-grupo_a_partidos_ordenado=desempatargrupo(grupo_a_partidos)
+grupo_a_definido=desempatargrupo(grupo_a_partidos)
 
 grupo_b_partidos=definirpuntosgrupo(grupo_b)
-grupo_b_partidos_ordenado=desempatargrupo(grupo_b_partidos)
+grupo_b_definido=desempatargrupo(grupo_b_partidos)
 
 grupo_c_partidos=definirpuntosgrupo(grupo_c)
-grupo_c_partidos_ordenado=desempatargrupo(grupo_c_partidos)
+grupo_c_definido=desempatargrupo(grupo_c_partidos)
 
 grupo_d_partidos=definirpuntosgrupo(grupo_d)
-grupo_d_partidos_ordenado=desempatargrupo(grupo_d_partidos)
+grupo_d_definido=desempatargrupo(grupo_d_partidos)
 
 grupo_e_partidos=definirpuntosgrupo(grupo_e)
-grupo_e_partidos_ordenado=desempatargrupo(grupo_e_partidos)
+grupo_e_definido=desempatargrupo(grupo_e_partidos)
 
 grupo_f_partidos=definirpuntosgrupo(grupo_f)
-grupo_f_partidos_ordenado=desempatargrupo(grupo_f_partidos)
+grupo_f_definido=desempatargrupo(grupo_f_partidos)
 
 grupo_g_partidos=definirpuntosgrupo(grupo_g)
-grupo_g_partidos_ordenado=desempatargrupo(grupo_g_partidos)
+grupo_g_definido=desempatargrupo(grupo_g_partidos)
 
 grupo_h_partidos=definirpuntosgrupo(grupo_h)
-grupo_h_partidos_ordenado=desempatargrupo(grupo_h_partidos)
+grupo_h_definido=desempatargrupo(grupo_h_partidos)
 
+#Como se enfrentan en las brakets según su posición en los grupos
+#1avs2b --|(octavos)
+#1cvs2d --|
+#         |  
+#         --- vs ---  | (cuartos)
+#         --- vs ---  |
+#         |           |
+#         |           |
+#1evs2f --|           | 
+#1gvs2h --|           --- vs ---|(semifinal)
+#                                --- vs ---  (final)
+#                     --- vs ---|(semifinal)
+#1bvs2a --|           |
+#1dvs2c --|           |          --- vs --- (tercerlugar)
+#         |           |
+#         |           |
+#         --- vs ---  | (cuartos)
+#         --- vs ---  |
+#         |
+#         | 
+#1fvs2e --|
+#1hvs2g --|(octavos)
 
+primeroctavo=[grupo_a_definido[0],grupo_b_definido[1]]
+segundooctavo=[grupo_c_definido[0],grupo_d_definido[1]]
+
+terceroctavo=[grupo_e_definido[0],grupo_f_definido[1]]
+cuartooctavo=[grupo_g_definido[0],grupo_h_definido[1]]
+
+quitooctavo=[grupo_b_definido[0],grupo_a_definido[1]]
+sextooctavo=[grupo_d_definido[0],grupo_c_definido[1]]
+
+septimooctavo=[grupo_f_definido[0],grupo_e_definido[1]]
+octavooctavo=[grupo_h_definido[0],grupo_g_definido[1]]
+print()
+print("Octavos de final -----------------------------")
+gprimer_octavo=ganadordelpartido(primeroctavo[0],primeroctavo[1])
+gsegundo_octavo=ganadordelpartido(segundooctavo[0],segundooctavo[1])
+gtercero_octavo=ganadordelpartido(terceroctavo[0],terceroctavo[1])
+gcuarto_octavo=ganadordelpartido(cuartooctavo[0],cuartooctavo[1])
+gquinto_octavo=ganadordelpartido(quitooctavo[0],quitooctavo[1])
+gsexto_octavo=ganadordelpartido(sextooctavo[0],sextooctavo[1])
+gseptimo_octavo=ganadordelpartido(septimooctavo[0],septimooctavo[1])
+goctavo_octavo=ganadordelpartido(octavooctavo[0],octavooctavo[1])
+print("Octavos de final -----------------------------")
+
+primercuarto=[gprimer_octavo,gsegundo_octavo]
+segundocuarto=[gtercero_octavo,gcuarto_octavo]
+print()
+tercercuarto=[gquinto_octavo,gsexto_octavo]
+cuartocuarto=[gseptimo_octavo,goctavo_octavo]
+print("Cuartos de final -----------------------------")
+gprimer_cuarto=ganadordelpartido(primercuarto[0],primercuarto[1])
+gsegundo_cuarto=ganadordelpartido(segundocuarto[0],segundocuarto[1])
+gtercer_cuarto=ganadordelpartido(tercercuarto[0],tercercuarto[1])
+gcuarto_cuarto=ganadordelpartido(cuartocuarto[0],cuartocuarto[1])
+print("Cuartos de final -----------------------------")
+primerasemifinal=[gprimer_cuarto,gsegundo_cuarto]
+segundasemifinal=[gtercer_cuarto,gcuarto_cuarto]
+print()
+print("Semifinal ------------------------------------")
+gprimera_semifinal=ganadordelpartido(primerasemifinal[0],primerasemifinal[1])
+gsegunda_semifinal=ganadordelpartido(segundasemifinal[0],segundasemifinal[1])
+print("Semifinal ------------------------------------")
+final=[gprimera_semifinal,gsegunda_semifinal]
+print()
+print("Final ----------------------------------------")
+g_final=ganadordelpartido(final[0],final[1])
+print("Final ----------------------------------------")
+print("^"*60)
+print()
+print(f"El ganador de esta edición del mundial es: {g_final}")
